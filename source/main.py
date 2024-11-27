19436a60cb689c5f0f1d45b80f0790f3267f22b1
GabrielNat1 <dcbiel846@gmail.com>
2024-11-27 17:57:39 -0300

    feat: initial config sprites for testing, create simple world.

--- source/main.py
+++ source/main.py
@@ -1,12 +1,17 @@
 import pygame, sys
+from gameplay.level import *
 from gameplay.settings import *
 
+sys.path.append(r'C:\RPG Eldoria\source')
+
 class Game:
-    def __init__(self) -> None:
+    def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGTH ))
         pygame.display.set_caption('Rpg Eldoria')
         self.clock = pygame.time.Clock()
+        
+        self.level = Level()
     
     def run(self):
         while True:
@@ -16,6 +21,7 @@ class Game:
                     sys.exit()
                     
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
             
