cc0a2034227c142bfa07d141127bad2e83be7f32
GabrielNat1 <dcbiel846@gmail.com>
2024-11-27 22:00:19 -0300

    feat: start creating move player, and colision

--- source/gameplay/level.py
+++ source/gameplay/level.py
@@ -2,6 +2,7 @@ import pygame
 from gameplay.settings import *
 from gameplay.tile import Tile
 from player.player import Player
+from gameplay.debug import *
 
 class Level:
     def __init__(self):
@@ -9,7 +10,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
         
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
     def create_map(self):
@@ -18,10 +19,12 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    Tile((x, y),[self.visible_sprites])
+                    Tile((x, y),[self.visible_sprites, self.obstacle_sprites])
                 if col == 'p':
-                    Player((x, y),[self.visible_sprites])
+                    self.player = Player((x, y),[self.visible_sprites])
     
     def run(self):
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
         
