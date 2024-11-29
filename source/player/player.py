1b2bb129a26cd44bd18aee5f7ae3c53b85c6c6d5
GabrielNat1 <dcbiel846@gmail.com>
2024-11-29 14:17:36 -0300

    feat: fix bugs

--- source/player/player.py
+++ source/player/player.py
@@ -36,38 +36,39 @@ class Player(pygame.sprite.Sprite):
             self.animations[animation] = import_folder(full_path)
     
     def input(self):
-        keys = pygame.key.get_pressed()
-        
+        if not self.attaking:
+             keys = pygame.key.get_pressed()
+
         #move 
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            self.direction.y = -1
-            self.status = 'up'
-        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            self.direction.y = 1
-            self.status = 'down'
-        else:
-            self.direction.y = 0
+             if keys[pygame.K_UP] or keys[pygame.K_w]:
+                self.direction.y = -1
+                self.status = 'up'
+             elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                 self.direction.y = 1
+                 self.status = 'down'
+             else:
+                 self.direction.y = 0
         
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.direction.x = 1
-            self.status = 'right'
-        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.direction.x = -1
-            self.status = 'left'
-        else:
-            self.direction.x = 0
+             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                  self.direction.x = 1
+                  self.status = 'right'
+             elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                   self.direction.x = -1
+                   self.status = 'left'
+             else:
+                   self.direction.x = 0
             
         #attack 
-        if keys[pygame.K_SPACE] and not self.attacking:
-            self.attacking = True
-            self.attack_time = pygame.time.get_ticks()
-            print('attack')
+             if keys[pygame.K_SPACE]:
+                  self.attacking = True
+                  self.attack_time = pygame.time.get_ticks()
+                  print('attack')
             
         #magic
-        if keys[pygame.K_LCTRL] and not self.attacking:
-            self.attacking = True
-            self.attack_time = pygame.time.get_ticks()
-            print('attack')
+             if keys[pygame.K_LCTRL]:
+                 self.attacking = True
+                 self.attack_time = pygame.time.get_ticks()
+                 print('magic')
     
     def get_status(self):
         
