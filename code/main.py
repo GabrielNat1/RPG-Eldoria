92e41f2468d4a04777afcd3e84dde8898788db77
GabrielNat1 <dcbiel846@gmail.com>
2024-12-14 23:37:34 -0300

    feat: screen

--- code/main.py
+++ code/main.py
@@ -3,36 +3,54 @@ from settings import *
 from level import Level
 
 class Game:
-	def __init__(self):
+    def __init__(self):
+        
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH), pygame.RESIZABLE)
+        pygame.display.set_caption('RPG Eldoria')
+        self.clock = pygame.time.Clock()
 
-		# general setup
-		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Zelda')
-		self.clock = pygame.time.Clock()
+        self.level = Level()
 
-		self.level = Level()
+        main_sound = pygame.mixer.Sound('../audio/main.ogg')
+        main_sound.set_volume(0.5)
+        main_sound.play(loops=-1)
 
-		# sound 
-		main_sound = pygame.mixer.Sound('../audio/main.ogg')
-		main_sound.set_volume(0.5)
-		main_sound.play(loops = -1)
-	
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
-				if event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_m:
-						self.level.toggle_menu()
+        self.fullscreen = False
 
-			self.screen.fill(WATER_COLOR)
-			self.level.run()
-			pygame.display.update()
-			self.clock.tick(FPS)
+    def toggle_fullscreen(self):
+        if not self.fullscreen:
+         
+            self.screen = pygame.display.set_mode((WIDTH, HEIGTH), pygame.FULLSCREEN)
+        else:
+            
+            self.screen = pygame.display.set_mode((WIDTH, HEIGTH), pygame.RESIZABLE)
+        self.fullscreen = not self.fullscreen
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+                        self.level.toggle_menu()
+                    if event.key == pygame.K_f:  
+                        self.toggle_fullscreen()
+
+                if event.type == pygame.VIDEORESIZE:
+                    
+                    WIDTH, HEIGTH = event.size
+                    self.screen = pygame.display.set_mode((WIDTH, HEIGTH), pygame.RESIZABLE)
+
+            self.screen.fill(WATER_COLOR)
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
 
 if __name__ == '__main__':
-	game = Game()
-	game.run()
+    game = Game()
+    game.run()
