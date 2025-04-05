4734b34ad3c69a5de8982eeb292db02d8994eff0
GabrielNat1 <dcbiel846@gmail.com>
2025-04-04 21:45:01 -0300

    feat: Fix commentaries in debug.py

--- code/debug.py
+++ code/debug.py
@@ -11,12 +11,14 @@ def debug(info, y=10, x=10):
     pygame.draw.rect(display_surface, 'Black', debug_rect)
     display_surface.blit(debug_surf, debug_rect)
 
-#def show_fps(clock, y=10, x=10):
-#    fps = int(clock.get_fps())
-#    debug(f"FPS: {fps}", y, x)
-#
-#def show_memory_usage(y=40, x=10):
-#    process = psutil.Process()
-#    memory_info = process.memory_info()
-#    memory_usage = memory_info.rss / (1024 * 1024)  # Convert to MB
-#    debug(f"Memory Usage: {memory_usage:.2f} MB", y, x)
+"""
+def show_fps(clock, y=10, x=10):
+    fps = int(clock.get_fps())
+    debug(f"FPS: {fps}", y, x)
+
+def show_memory_usage(y=40, x=10):
+    process = psutil.Process()
+    memory_info = process.memory_info()
+    memory_usage = memory_info.rss / (1024 * 1024)  # Convert to MB
+    debug(f"Memory Usage: {memory_usage:.2f} MB", y, x)
+"""
